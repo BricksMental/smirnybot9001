99a71b28fc97519f85f212566f2eda884b51df1a
BricksMental <bricks.mental@gmail.com>
2022-10-10 23:13:13 +0200

    Improve color table and test. Store both directions of the mapping in one dict and hope for the best ;-)

--- smirnybot9001/color_table.py
+++ smirnybot9001/color_table.py
@@ -4,6 +4,16 @@ from smirnybot9001.util import get_with_user_agent
 
 COLOR_TABLE_URL = 'https://www.bricklink.com/catalogColors.asp'
 
+COLOR_TABLE = None
+
+
+def get_color_table():
+    global COLOR_TABLE
+    if COLOR_TABLE is None:
+        COLOR_TABLE = scrape_color_table()
+    return COLOR_TABLE
+
+
 
 def scrape_color_table():
     r = get_with_user_agent(COLOR_TABLE_URL)
@@ -11,7 +21,10 @@ def scrape_color_table():
 
 
 def parse_color_table(color_table_html):
-    soup = BeautifulSoup(color_table_html)
+
+    ret = dict()
+
+    soup = BeautifulSoup(color_table_html, 'html.parser')
     t = soup.find('table', attrs={'id': 'id-main-legacy-table'})
     tables = t.td.find_all('table')
     # fetch all subtables of color groups
@@ -22,5 +35,9 @@ def parse_color_table(color_table_html):
             id_td = color_row.contents[0]
             color_id = int(id_td.font.text)
             name_td = color_row.contents[3]
-            color_name = name_td.font.text.strip()
-            print(color_name, color_id)
+            color_name = name_td.font.text.strip().lower()
+            # print(color_name, color_id)
+            ret[color_name] = color_id
+            ret[color_id] = color_name
+
+    return ret
--- tests/test_color_table.py
+++ tests/test_color_table.py
@@ -1,9 +1,11 @@
 from pathlib import Path
+import pickle
 
 from smirnybot9001.color_table import parse_color_table, scrape_color_table
 
 MYDIR = Path(__file__).resolve().parent
 COLOR_TABLE_HTML = MYDIR / 'bricklink_color_table.html'
+COLOR_TABLE = MYDIR / 'bricklink_color_table.pickle'
 
 
 def test_scrape():
@@ -13,3 +15,7 @@ def test_scrape():
 def test_parse():
     r = open(COLOR_TABLE_HTML).read()
     ct = parse_color_table(r)
+    # with COLOR_TABLE.open('wb') as ctf:
+    #    pickle.dump(ct, ctf)
+    expected = pickle.load(COLOR_TABLE.open('rb'))
+    assert ct == expected
